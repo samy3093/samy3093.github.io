128f3b40b7240348caa6de94fa010e27c260cdfa
karuppasaamy <karuppasamy3093@gmail.com>
2023-11-19 17:34:08 +0530

    update

--- app/src/app/board/game/game.component.ts
+++ app/src/app/board/game/game.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-interface Card {
+interface Mole {
   id: number;
-  value: string;
-  flipped: boolean;
+  active: boolean;
 }
 
 @Component({
@@ -12,76 +11,37 @@ interface Card {
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit {
-  cards: Card[] = [];
-  selectedCards: Card[] = [];
-  moves: number = 0;
+  moles: Mole[] = [];
+  score: number = 0;
 
   ngOnInit(): void {
     this.initializeGame();
   }
 
   initializeGame(): void {
-    const values = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
-    const allCards: Card[] = [];
-
-    // Duplicate values to create matching pairs
-    for (let i = 0; i < values.length; i++) {
-      allCards.push({ id: i * 2, value: values[i], flipped: false });
-      allCards.push({ id: i * 2 + 1, value: values[i], flipped: false });
-    }
-
-    // Shuffle the cards
-    this.cards = this.shuffleArray(allCards);
+    this.moles = Array.from({ length: 9 }, (_, index) => ({ id: index, active: false }));
+    this.startGame();
   }
 
-  shuffleArray(array: any[]): any[] {
-    let currentIndex = array.length;
-    let randomIndex: number;
-
-    while (currentIndex !== 0) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-
-      [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
-    }
-
-    return array;
+  startGame(): void {
+    setInterval(() => {
+      this.showRandomMole();
+    }, 1000);
   }
 
-  onCardClick(card: Card): void {
-    if (card.flipped || this.selectedCards.length === 2) {
-      return;
-    }
+  showRandomMole(): void {
+    const randomIndex = Math.floor(Math.random() * this.moles.length);
+    this.moles[randomIndex].active = true;
 
-    card.flipped = true;
-    this.selectedCards.push(card);
-
-    if (this.selectedCards.length === 2) {
-      this.moves++;
-      setTimeout(() => {
-        this.checkForMatch();
-      }, 1000);
-    }
+    setTimeout(() => {
+      this.moles[randomIndex].active = false;
+    }, 800);
   }
 
-  checkForMatch(): void {
-    const [card1, card2] = this.selectedCards;
-
-    if (card1.value === card2.value) {
-      card1.flipped = true;
-      card2.flipped = true;
-    } else {
-      card1.flipped = false;
-      card2.flipped = false;
-    }
-
-    this.selectedCards = [];
-
-    // Check for game completion
-    if (this.cards.every(card => card.flipped)) {
-      alert(`Congratulations! You won in ${this.moves} moves.`);
-      this.initializeGame();
-      this.moves = 0;
+  onMoleClick(mole: Mole): void {
+    if (mole.active) {
+      mole.active = false;
+      this.score++;
     }
   }
 }
